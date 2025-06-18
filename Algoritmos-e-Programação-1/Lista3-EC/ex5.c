2ca4a8cae98df898ccf91f032b8b92285c403cb6
Lucas <lucas.alfra@gmail.com>
2025-06-18 01:26:57 -0300

    Atualizando .gitignore e removendo arquivos ignorados

--- Algoritmos-e-Programação-1/Lista3-EC/ex5.c
+++
@@ -1,7 +0,0 @@
-/*
-05 - O número 3025 possui a seguinte característica:
-20 + 25 = 55
-55^2    = 3025
-
-Fazer um algoritmo que dado um número de 4 dígitos calcule e escreva se ele possui ou não esta característica.
-*/
