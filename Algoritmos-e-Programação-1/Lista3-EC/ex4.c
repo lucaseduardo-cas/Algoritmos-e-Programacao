eef3b53d1c80c35a4feb8ab42a936bf0af928332
Lucas <lucas.alfra@gmail.com>
2025-06-18 01:18:11 -0300

    Adicionando novos exercicios de Lista3-EC

---
+++ Algoritmos-e-Programação-1/Lista3-EC/ex4.c
@@ -0,0 +1,15 @@
+/*
+04 - Escrever um algoritmo que leia 3 números inteiros e que mostre o maior deles, supondo que todos sejam distintos.
+*/
+
+#include <stdio.h>
+
+int main () {
+    int n = -4;
+
+    n = -(n);
+
+    printf("%d\n", n);
+
+    return 0;
+}
---
+++ Algoritmos-e-Programação-1/Lista3-EC/ex5.c
@@ -0,0 +1,7 @@
+/*
+05 - O número 3025 possui a seguinte característica:
+20 + 25 = 55
+55^2    = 3025
+
+Fazer um algoritmo que dado um número de 4 dígitos calcule e escreva se ele possui ou não esta característica.
+*/
