d4ba99962b3620559240cb5a35a4921979451a76
Lucas <lucas.alfra@gmail.com>
2025-06-26 01:03:25 -0300

    Enviando novos exercícios .c

---
+++ Algoritmos-e-Programação-1/Lista3-EC/ex18.c
@@ -0,0 +1,15 @@
+/*
+18 - Desenvolver um algoritmo para calcular e imprimir o preço final de um carro. O valor do preço inicial de fábrica é fornecido por
+um meio de entrada. O carro pode ter as seguintes opções:
+(a) (S,N)Ar condicionado: R$ 1750,00
+(b) (S,N)Pintura Metálica: R$ 800,00
+(c) (S,N)Vidro Elétrico: R$ 1200,00
+(d) (S,N)Direção Hidráulica: R$ 2000,00. 
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
