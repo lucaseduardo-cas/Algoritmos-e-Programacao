28daf85a0889723f5da3aae987e5f134dabb6190
Lucas <lucas.alfra@gmail.com>
2025-06-26 20:49:37 -0300

    Adicionado ex18.c

--- Algoritmos-e-Programação-1/Lista3-EC/ex18.c
+++ Algoritmos-e-Programação-1/Lista3-EC/ex18.c
@@ -10,6 +10,63 @@ um meio de entrada. O carro pode ter as seguintes opções:
 #include <stdio.h>
 
 int main() {
+    float precoInicial = 0.0, precoFinal = 0.0, precoOpcional = 0.0;
+    char a, b, c, d;
+    
+    printf("=========== CALCULADORA DE PREÇO DE VENDA DE VEÍCULO ===========\n\n");
+    printf("Preço inicial de fábrica: ");
+    scanf("%f", &precoInicial);
+    printf("\nEscolha os opcionais:\n");
+    printf("(a) Ar condicionado: ..... : R$ 1.750,00 (S,N): ");
+    scanf(" %c", &a);
+    if (a != 'S' && a != 's' && a != 'N' && a != 'n') {
+        printf("\nOpção inválida! Digite 'S' ou 'N'\n");
+        return 0;
+    }
+
+    printf("(b) Pintura Metálica: .... : R$   800,00 (S,N): ");
+    scanf(" %c", &b);
+    if (b != 'S' && b != 's' && b != 'N' && b!= 'n') {
+        printf("\nOpção inválida! Digite 'S' ou 'N'\n");
+        return 0;
+    }
+
+    printf("(c) Vidro Elétrico: ...... : R$ 1.200,00 (S,N): ");
+    scanf(" %c", &c);
+    if (c != 'S' && c != 's' && c != 'N' && c != 'n') {
+        printf("\nOpção inválida! Digite 'S' ou 'N'\n");
+        return 0;
+    }
+    printf("(d) Direção Hidráulica ... : R$ 2.000,00 (S,N): ");
+    scanf(" %c", &d);
+    if (d != 'S' && d != 's' && d != 'N' && d != 'n') {
+        printf("\nOpção inválida! Digite 'S' ou 'N'\n");
+        return 0;
+    }
+
+    if (a == 'S' || a == 's') {
+        precoOpcional = 1750.0;
+    } else {
+        precoOpcional = precoOpcional;
+    }
+    if (b == 'S' || b == 's') {
+        precoOpcional += 800.0;
+    } else {
+        precoOpcional = precoOpcional;
+    }
+    if (c == 'S' || b == 's') {
+        precoOpcional += 1200.0;
+    } else {
+        precoOpcional = precoOpcional;
+    } if (d == 'S' || d == 's') {
+        precoOpcional += 2000.0;
+    } else {
+        precoOpcional = precoOpcional;
+    } 
+
+    precoFinal = precoInicial + precoOpcional;
+
+    printf("\nPreço final: %'.2f\n\n", precoFinal);
 
     return 0;
 }
