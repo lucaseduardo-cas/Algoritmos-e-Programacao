d9b2c9303151111f8c31c32dbc3b22df1fb2f96e
lucaseduardo-cas <lucas.alfra@gmail.com>
2025-06-29 22:33:23 -0300

    Adicionado ex1.c, ex2.c e ex3.c

--- Algoritmos-e-Programação-1/Lista4-EC/ex1.c
+++ Algoritmos-e-Programação-1/Lista4-EC/ex1.c
@@ -1,6 +1,24 @@
+/*
+1 – Faça um programa para ler dois números inteiros M e N e determinar se M é divisível por N. 
+*/
+
 #include <stdio.h>
 
 int main(void) {
+    int m, n, resultado;
+
+    printf("Leia dois números inteiros:\n");
+    printf("1° número: ");
+    scanf("%d", &m);
+    printf("2° número: ");
+    scanf("%d", &n);
+
+    if (m % n == 0) {
+        resultado = m / n;
+        printf("\n%d é divisível por %d!\nResultado = %d\n\n", m, n, resultado);
+    } else {
+        printf("\n%d NÃO é divisível por %d!\n\n", m, n);
+    }
 
     return 0;
 }
--- Algoritmos-e-Programação-1/Lista4-EC/ex2.c
+++ Algoritmos-e-Programação-1/Lista4-EC/ex2.c
@@ -1,6 +1,38 @@
+/*
+2- A nota de um aluno em uma disciplina é a média ponderada das notas de suas duas avaliações parciais. A primeira
+avaliação parcial tem peso 2 e a segunda avaliação parcial tem peso 3. Se a média do aluno for maior ou igual a sete, o aluno
+está aprovado. Se a média do aluno for menor que três, o aluno está reprovado. Se a média do aluno for menor que sete e
+maior ou igual a três, o aluno irá para a recuperação. Faça um programa para calcular a média de um aluno em uma
+determinada disciplina e informar o seu estado final. O programa deverá exibir a seguinte frase:
+O aluno _____________ obteve média _________ e está ______________. 
+*/
+
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
+    float nota1, nota2, mediaP;
+    char nome[20];
 
+    printf("Nome do aluno: ");
+    scanf("%s", nome);
+    printf("Nota 1: ");
+    scanf("%f", &nota1);
+    printf("Nota 2: ");
+    scanf("%f", &nota2);
+
+    mediaP = ((nota1 * 2.0) + (nota2 * 3.0)) / 5.0;
+
+    printf("\nO aluno %s obteve média %.2f e está", nome, mediaP);
+
+    if (mediaP >= 7.0) {
+        printf(" aprovado!");
+    } else if (mediaP < 3.0) {
+        printf(" reprovado!");
+    } else {
+        printf(" recuperação!");
+    }
+    
+    printf("\n\n");
     return 0;
 }
