e5aec3fe62966348c263aeaabcaaf5b1cc85135e
Lucas <lucas.alfra@gmail.com>
2025-07-22 17:40:29 -0300

    Atualizado ex-30.c

--- Algoritmos-e-Programação-2/Exercicios-Vetores-UNESP/Ex-30.c
+++ Algoritmos-e-Programação-2/Exercicios-Vetores-UNESP/Ex-30.c
@@ -1,6 +1,6 @@
 /*
-30. Fac¸a um programa que leia dois vetores de 10 elementos. Crie um vetor que seja a
-intersecc¸ao entre os 2 vetores anteriores, ou seja, que contém apenas os números que
+30. Faça um programa que leia dois vetores de 10 elementos. Crie um vetor que seja a
+intersecção entre os 2 vetores anteriores, ou seja, que contém apenas os números que
 estao em ambos os vetores. Não deve conter números repitidos
 */
 
@@ -8,43 +8,73 @@ estao em ambos os vetores. Não deve conter números repitidos
 
 int main(void) {
     int v1[10], v2[10], v3[10];
-    int i, j;
+    int i, j, k;
 
-    i = 0;
-    while (i < 10) {
-        v1[i] = 1;
-        i++;
-    }
-
-    i = 0;
-    while (i < 10) {
-        v2[i] = 1;
-        i++;
-    }
-
-    /*
     printf("Digite 10 números inteiros:\n");
     for (i = 0; i < 10; i++) {
         printf("%2d° número: ", i + 1);
         scanf("%d", &v1[i]);
     }
 
-    printf("Digite 10 números inteiros:\n");
+    printf("\nDigite 10 números inteiros:\n");
     for (i = 0; i < 10; i++) {
         printf("%2d° número: ", i + 1);
         scanf("%d", &v2[i]);
     }
-    */
-
+    
+    k = 0;
     for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+         for (j = 0; j < 10; j++) {
             if (v1[i] == v2[j]) {
-                v3[i] = v1[i];
+                v3[k] = v1[i];
+                k++;
                 break;
             } else {
                 v3[i] = 0;
             }
         }
     }
+    
+    int v4[k];
+    j = 0;
+    v3[5];
+    for (i = 0; i < k; i++)  {
+        if (v3[i] != 0) {
+            v4[j] = v3[i];
+            j++;
+        }
+    }
+
+    int contador = 0;
+    for (i = 0; i < k; i++) {
+        if(i > 0) {
+            for (j = 0; j < k - 1; j++) {
+                if (v4[i] == v4[j] && j != i) {
+                    v4[i] = 0;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        }
+        if (v4[i] != 0) {
+            contador++;
+        }
+    }
+
+    int v5[contador];
+    int l = 0;
+    for (i = 0; i < k; i++) {
+        if (v4[i] != 0) {
+            v5[l] = v4[i];
+            l++;
+        }
+    }
+    printf("\nIntersecção entre os vetores A e B:\n");
+    for (i = 0; i < contador; i++) {
+        printf("%d\n", v5[i]);
+    }
+
+    printf("\n\n");
     return 0;
 }
