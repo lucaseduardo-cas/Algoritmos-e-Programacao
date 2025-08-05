d5011832295f6744e598a8bd51241a3425563ced
Lucas <lucas.alfra@gmail.com>
2025-08-05 11:05:47 -0300

    Organização do repositório

---
+++ Algoritmos-e-Programação-2/Exercicios-Matrizes-UNESP/ex5.c
@@ -0,0 +1,6 @@
+/*
+5. Leia uma matriz 5 x 5. Leia tambem um valor ´ X. O programa devera fazer uma busca ´
+desse valor na matriz e, ao final, escrever a localizac¸ao (linha e coluna) ou uma mensa- ˜
+gem de “nao encontrado”. 
+*/
+
